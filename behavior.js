9932c431f7a5fd8f2bf017bc5d1b591de18b5fa5
Camilapinz18 <83082880+Camilapinz18@users.noreply.github.com>
2022-09-05 09:53:49 -0500

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,154 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	var thumbnailElement = document.getElementById("smart_thumbnail");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail2");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail3");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail4");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail5");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail6");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail7");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail8");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail9");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail10");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail11");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail12");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail13");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail14");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail15");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail16");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail17");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	var thumbnailElement = document.getElementById("smart_thumbnail19");
+	thumbnailElement.addEventListener("click", function() {
+ 		if (this.className == "small")
+    		this.className = "";
+  		else
+   			this.className = "small";
+	});
+
+	/*--------------------------------------------*/
+
+	var buttonElement = document.getElementById("buyButton");
+	buttonElement.addEventListener("click", function() {
+ 		alert('Has acurred a problem, try later!');
+	});
+
+});
